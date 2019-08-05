42f739e85ba85a4b682cec4a3a36c3d7a52fc58c
gq <2282363130@qq.com>
2019-08-05 18:48:26 +0800

    add Transformer

---
+++ Transformer/transformer.py
@@ -0,0 +1,217 @@
+# -*- coding: UTF-8 -*-
+import tensorflow as tf
+from tensorflow.python.estimator.canned import head as head_lib
+from tensorflow.python.ops.losses import losses
+from utils import feedforward, multihead_attention, layer_normalization
+import collections
+
+'''
+A tensorflow implementation of Transformer
+
+Ashish Vasmani et all.  "Attention is All You Need,"  In NIPS,2017.
+
+'''
+
+class TransformerNetwork(object):
+    def __init__(self, num_units, num_blocks, num_heads, max_len, dropout_rate, pos_fixed=True, l2_reg=0.0):
+        self.num_units = num_units        # embedding_size
+        self.num_blocks = num_blocks      # the number of multi-head attention we use
+        self.num_heads = num_heads
+        self.max_len = max_len              # the max length of the sequence
+        self.dropout_keep_prob = 1. - dropout_rate
+        self.position_encoding_matrix = None
+        self.pos_fixed = pos_fixed
+        self.l2_reg = l2_reg
+
+    def  get_position_encoding(self, inputs, scope="pos_embedding/", reuse=None, dtype=tf.float32):
+        '''
+        Args:
+            inputs: sequence embeddings, shape: (batch_size , max_len, embedding_size)
+        Return:
+            Output sequences which has the same shape with inputs
+        '''
+        #E = inputs.get_shape().as_list()[-1]  # static
+        #N, T = tf.shape(inputs)[0], tf.shape(inputs)[1]  # dynamic
+        with tf.variable_scope(scope, reuse=reuse):
+            if self.position_encoding_matrix is None:
+                encoded_vec = np.array(
+                    [pos / np.power(10000, 2 * i / self.num_units) for pos in range(self.max_len) for i in
+                     range(self.num_units)])
+                encoded_vec[::2] = np.sin(encoded_vec[::2])     #从index为0开始，下一个元素index+2
+                encoded_vec[1::2] = np.cos(encoded_vec[1::2])
+                encoded_vec = tf.convert_to_tensor(encoded_vec.reshape([self.max_len, self.num_units]), dtype=dtype)
+                self.position_encoding_matrix = encoded_vec  # (max_len, num_units)
+
+            N = tf.shape(inputs)[0]     # batch_size
+            T = tf.shape(inputs)[1]     # max_len
+            position_ind = tf.tile(tf.expand_dims(tf.range(T), 0), [N, 1])  # (batch_size , max_len)
+            position_encoding = tf.nn.embedding_lookup(self.position_encoding_matrix,
+                                                       position_ind)  # (batch_size, len, num_units)
+        return position_encoding
+
+
+    def __call__(self, inputs, mask):
+        '''
+        Args:
+            inputs: sequence embeddings (item_embeddings +  pos_embeddings) shape: (batch_size , max_len, embedding_size)
+            mask:  deal with mask shape: (batch_size, max_len, 1)
+        Return:
+            Output sequences which has the same shape with inputs
+        '''
+        if self.pos_fixed:  # use sin /cos positional embedding
+            position_encoding = self.get_position_encoding(inputs)  # (batch_size, len, num_units)
+
+        inputs += position_encoding
+        inputs *= mask
+        for i in range(self.num_blocks):
+            with tf.variable_scope("num_blocks_%d" % i):
+                # Self-attention
+                inputs = multihead_attention(queries=layer_normalization(inputs),
+                                             keys=inputs,
+                                             num_units=self.num_units,
+                                             num_heads=self.num_heads,
+                                             dropout_keep_prob=self.dropout_keep_prob,
+                                             causality=True,
+                                             scope="self_attention")
+
+                # Feed forward
+                inputs = feedforward(layer_normalization(inputs), num_units=[self.num_units, self.num_units],
+                                     dropout_keep_prob=self.dropout_keep_prob)
+
+                inputs *= mask
+        outputs = layer_normalization(inputs)  # (batch_size, max_len, num_units)
+        return outputs
+
+def build_deep_layers(net, params):
+    # Build the hidden layers, sized according to the 'hidden_units' param.
+
+    for layer_id, num_hidden_units in enumerate(params['hidden_units']):
+        net = tf.layers.dense(net, units=num_hidden_units, activation=tf.nn.relu,
+                              kernel_initializer=tf.glorot_uniform_initializer())
+
+    return net
+
+
+def _check_fm_columns(feature_columns):
+    if isinstance(feature_columns, collections.Iterator):
+        feature_columns = list(feature_columns)
+    column_num = len(feature_columns)
+    if column_num < 2:
+        raise ValueError('feature_columns must have as least two elements.')
+    dimension = -1
+    for column in feature_columns:
+        if dimension != -1 and column.dimension != dimension:
+            raise ValueError('fm_feature_columns must have the same dimension.')
+        dimension = column.dimension
+    return column_num, dimension
+
+
+def build_cross_layers(x0, params):
+    num_layers = params['num_cross_layers']
+    x = x0
+    for i in range(num_layers):
+        x = cross_layer(x0, x, 'cross_{}'.format(i))
+    return x
+
+
+def cross_layer(x0, x, name):
+    with tf.variable_scope(name):
+        input_dim = x0.get_shape().as_list()[1]
+        w = tf.get_variable("weight", [input_dim], initializer=tf.truncated_normal_initializer(stddev=0.01))
+        b = tf.get_variable("bias", [input_dim], initializer=tf.truncated_normal_initializer(stddev=0.01))
+        xb = tf.tensordot(tf.reshape(x, [-1, 1, input_dim]), w, 1)
+        return x0 * xb + b + x
+
+def attention_layer(querys, keys, keys_id):
+    """
+        queries:     [Batchsize, 1, embedding_size]
+        keys:        [Batchsize, max_seq_len, embedding_size]  max_seq_len is the number of keys(e.g. number of clicked creativeid for each sample)
+        keys_id:     [Batchsize, max_seq_len]
+    """
+
+    keys_length = tf.shape(keys)[1] # padded_dim
+    embedding_size = querys.get_shape().as_list()[-1]
+    keys = tf.reshape(keys, shape=[-1, keys_length, embedding_size])
+    querys = tf.reshape(tf.tile(querys, [1, keys_length, 1]), shape=[-1, keys_length, embedding_size])
+
+    net = tf.concat([keys, keys - querys, querys, keys*querys], axis=-1)
+    for units in [32,16]:
+      net = tf.layers.dense(net, units=units, activation=tf.nn.relu)
+    att_wgt = tf.layers.dense(net, units=1, activation=tf.sigmoid)        # shape(batch_size, max_seq_len, 1)
+    outputs = tf.reshape(att_wgt, shape=[-1, 1, keys_length], name="weight")  #shape(batch_size, 1, max_seq_len)
+    
+    scores = outputs
+    #key_masks = tf.expand_dims(tf.cast(keys_id > 0, tf.bool), axis=1)  # shape(batch_size, 1, max_seq_len) we add 0 as padding
+    # tf.not_equal(keys_id, '0')  如果改成str
+    key_masks = tf.expand_dims(tf.not_equal(keys_id, '0'), axis=1)
+    paddings = tf.ones_like(scores) * (-2 ** 32 + 1)
+    scores = tf.where(key_masks, scores, paddings)
+    scores = scores / (embedding_size ** 0.5)       # scale
+    scores = tf.nn.softmax(scores)
+    outputs = tf.matmul(scores, keys)    #(batch_size, 1, embedding_size)
+    outputs = tf.reduce_sum(outputs, 1, name="attention_embedding")   #(batch_size, embedding_size)
+    
+    return outputs
+
+
+def din_model_fn(features, labels, mode, params):
+    net = tf.feature_column.input_layer(features, params['feature_columns'])
+
+    last_click_creativeid = tf.string_to_hash_bucket_fast(features["user_click_creatives_att"], 200000)
+    creativeid_embeddings = tf.get_variable(name="attention_creativeid_embeddings", dtype=tf.float32,
+                                            shape=[200000, 20])
+    last_click_creativeid_emb = tf.nn.embedding_lookup(creativeid_embeddings, last_click_creativeid)
+
+    last_click_productid = tf.string_to_hash_bucket_fast(features["user_click_products_att"], 40000)
+    productid_embeddings = tf.get_variable(name="attention_productid_embeddings", dtype=tf.float32,
+                                        shape=[40000, 20])
+    last_click_productid_emb = tf.nn.embedding_lookup(productid_embeddings, last_click_productid)
+
+    his_click_emb = tf.concat([last_click_creativeid_emb, last_click_productid_emb], 2)  # (batch_size,10,emb_size*2)
+
+    transformerNetwork_click = TransformerNetwork(params['transformer_num_units'], params['num_blocks'],
+                                                  params['num_heads'],
+                                                  max_len=10, dropout_rate=params['dropout_rate'], pos_fixed=True)
+    mask_click = tf.expand_dims(tf.to_float(tf.cast(tf.not_equal(features["user_click_creatives_att"], "0"),
+                                                    tf.float32)), -1)  # (batch_size, 10, 1)
+
+    transformer_click_outputs = transformerNetwork_click(his_click_emb, mask_click)  # (batch_size, max_len, num_units)
+    transformer_click_outputs = tf.reshape(tf.reduce_sum(transformer_click_outputs, 1),
+                                           shape=[-1, params['transformer_num_units']])
+
+
+    last_deep_layer = build_deep_layers(net, params)
+    last_cross_layer = build_cross_layers(net, params)
+
+    last_layer = tf.concat([last_deep_layer, last_cross_layer, transformer_click_outputs], 1)
+
+
+    # head = tf.contrib.estimator.binary_classification_head(loss_reduction=losses.Reduction.SUM)
+    head = head_lib._binary_logistic_or_multi_class_head(  # pylint: disable=protected-access
+        n_classes=2, weight_column=None, label_vocabulary=None, loss_reduction=losses.Reduction.SUM)
+    logits = tf.layers.dense(last_layer, units=head.logits_dimension,
+                             kernel_initializer=tf.glorot_uniform_initializer())
+    optimizer = tf.train.AdagradOptimizer(learning_rate=params['learning_rate'])
+    preds = tf.sigmoid(logits)
+    user_id = features['user_id']
+    label = features['label']
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        predictions = {
+            'probabilities': preds,
+            'user_id': user_id,
+            'label': label
+        }
+        export_outputs = {
+            'regression': tf.estimator.export.RegressionOutput(predictions['probabilities'])
+        }
+        return tf.estimator.EstimatorSpec(mode, predictions=predictions, export_outputs=export_outputs)
+
+    return head.create_estimator_spec(
+        features=features,
+        mode=mode,
+        labels=labels,
+        logits=logits,
+        train_op_fn=lambda loss: optimizer.minimize(loss, global_step=tf.train.get_global_step())
+    )
+
---
+++ Transformer/utils.py
@@ -0,0 +1,201 @@
+import tensorflow as tf
+
+def dice(_x, axis=-1, epsilon=0.0000001, name=''):
+  with tf.variable_scope(name, reuse=tf.AUTO_REUSE):
+    alphas = tf.get_variable('alpha'+name, _x.get_shape()[-1],
+                         initializer=tf.constant_initializer(0.0),
+                         dtype=tf.float32)
+    input_shape = list(_x.get_shape())
+
+    reduction_axes = list(range(len(input_shape)))
+    del reduction_axes[axis]
+    broadcast_shape = [1] * len(input_shape)
+    broadcast_shape[axis] = input_shape[axis]
+
+  # case: train mode (uses stats of the current batch)
+  mean = tf.reduce_mean(_x, axis=reduction_axes)
+  brodcast_mean = tf.reshape(mean, broadcast_shape)
+  std = tf.reduce_mean(tf.square(_x - brodcast_mean) + epsilon, axis=reduction_axes)
+  std = tf.sqrt(std)
+  brodcast_std = tf.reshape(std, broadcast_shape)
+  x_normed = (_x - brodcast_mean) / (brodcast_std + epsilon)
+  # x_normed = tf.layers.batch_normalization(_x, center=False, scale=False)
+  x_p = tf.sigmoid(x_normed)
+
+  return alphas * (1.0 - x_p) * _x + x_p * _x
+
+def prelu(_x, scope=''):
+    """parametric ReLU activation"""
+    with tf.variable_scope(name_or_scope=scope, default_name="prelu"):
+        _alpha = tf.get_variable("prelu_"+scope, shape=_x.get_shape()[-1],
+                                 dtype=_x.dtype, initializer=tf.constant_initializer(0.1))
+        return tf.maximum(0.0, _x) + _alpha * tf.minimum(0.0, _x)
+
+def layer_normalization(inputs,
+              epsilon=1e-8,
+              scope="ln",
+              reuse=None):
+    '''Applies layer normalization.
+
+    Args:
+      inputs: A tensor with 2 or more dimensions, where the first dimension has
+        `batch_size`.
+      epsilon: A floating number. A very small number for preventing ZeroDivision Error.
+      scope: Optional scope for `variable_scope`.
+      reuse: Boolean, whether to reuse the weights of a previous layer
+        by the same name.
+
+    Returns:
+      A tensor with the same shape and data dtype as `inputs`.
+    '''
+                 #auto_reuse or reuse
+    with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
+        inputs_shape = inputs.get_shape()
+        params_shape = inputs_shape[-1:]
+
+        mean, variance = tf.nn.moments(inputs, [-1], keep_dims=True)
+        beta = tf.Variable(tf.zeros(params_shape))
+        gamma = tf.Variable(tf.ones(params_shape))
+        normalized = (inputs - mean) / ((variance + epsilon) ** (.5))
+        outputs = gamma * normalized + beta
+
+    return outputs
+
+
+def multihead_attention(queries,
+                        keys,
+                        num_units=None,
+                        num_heads=8,
+                        dropout_keep_prob=1.0,
+                        causality=False,
+                        scope="multihead_attention",
+                        reuse=None,
+                        with_qk=False):
+    '''
+    Applies multihead attention.
+
+    Args:
+        queries: A 3d tensor with shape of [N, T_q, C_q].
+        keys: A 3d tensor with shape of [N, T_k, C_k].
+        num_units: A scalar. Attention size.
+        dropout_rate: A floating point number.
+        is_training: Boolean. Controller of mechanism for dropout.
+        causality: Boolean. If true, units that reference the future are masked.
+        num_heads: An int. Number of heads.
+        scope: Optional scope for `variable_scope`.
+        reuse: Boolean, whether to reuse the weights of a previous layer
+          by the same name.
+
+    Returns
+        A 3d tensor with shape of (N, T_q, C)
+    '''
+    with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
+        # Set the fall back option for num_units
+        if num_units is None:
+            num_units = queries.get_shape().as_list[-1]
+
+        # Linear projections
+        # Q = tf.layers.dense(queries, num_units, activation=tf.nn.relu) # (N, T_q, C)
+        # K = tf.layers.dense(keys, num_units, activation=tf.nn.relu) # (N, T_k, C)
+        # V = tf.layers.dense(keys, num_units, activation=tf.nn.relu) # (N, T_k, C)
+        Q = tf.layers.dense(queries, num_units, activation=None)  # (N, T_q, C)
+        K = tf.layers.dense(keys, num_units, activation=None)  # (N, T_k, C)
+        V = tf.layers.dense(keys, num_units, activation=None)  # (N, T_k, C)
+
+        # Split and concat
+        Q_ = tf.concat(tf.split(Q, num_heads, axis=2), axis=0)  # (h*N, T_q, C/h)
+        K_ = tf.concat(tf.split(K, num_heads, axis=2), axis=0)  # (h*N, T_k, C/h)
+        V_ = tf.concat(tf.split(V, num_heads, axis=2), axis=0)  # (h*N, T_k, C/h)
+
+        # Multiplication
+        outputs = tf.matmul(Q_, tf.transpose(K_, [0, 2, 1]))  # (h*N, T_q, T_k)
+
+        # Scale
+        outputs = outputs / (K_.get_shape().as_list()[-1] ** 0.5)
+
+        # Key Masking
+        key_masks = tf.sign(tf.abs(tf.reduce_sum(keys, axis=-1)))  # (N, T_k)
+        key_masks = tf.tile(key_masks, [num_heads, 1])  # (h*N, T_k)
+        key_masks = tf.tile(tf.expand_dims(key_masks, 1), [1, tf.shape(queries)[1], 1])  # (h*N, T_q, T_k)
+
+        paddings = tf.ones_like(outputs) * (-2 ** 32 + 1)
+        outputs = tf.where(tf.equal(key_masks, 0), paddings, outputs)  # (h*N, T_q, T_k)
+
+        # Causality = Future blinding, for inference
+        if causality:
+            diag_vals = tf.ones_like(outputs[0, :, :])  # (T_q, T_k)
+            try:
+                tril = tf.contrib.linalg.LinearOperatorLowerTriangular(diag_vals).to_dense()  # (T_q, T_k)
+            except:
+                tril = tf.contrib.linalg.LinearOperatorTriL(diag_vals).to_dense()  # (T_q, T_k)
+            masks = tf.tile(tf.expand_dims(tril, 0), [tf.shape(outputs)[0], 1, 1])  # (h*N, T_q, T_k)
+
+            paddings = tf.ones_like(masks) * (-2 ** 32 + 1)
+            outputs = tf.where(tf.equal(masks, 0), paddings, outputs)  # (h*N, T_q, T_k)
+
+        # Activation
+        outputs = tf.nn.softmax(outputs)  # (h*N, T_q, T_k)
+
+        # Query Masking
+        query_masks = tf.sign(tf.abs(tf.reduce_sum(queries, axis=-1)))  # (N, T_q)
+        query_masks = tf.tile(query_masks, [num_heads, 1])  # (h*N, T_q)
+        query_masks = tf.tile(tf.expand_dims(query_masks, -1), [1, 1, tf.shape(keys)[1]])  # (h*N, T_q, T_k)
+        outputs *= query_masks  # broadcasting. (N, T_q, C)
+
+        # Dropouts
+        outputs = tf.nn.dropout(outputs, keep_prob=dropout_keep_prob)
+
+        # Weighted sum
+        outputs = tf.matmul(outputs, V_)  # ( h*N, T_q, C/h)
+
+        # Restore shape
+        outputs = tf.concat(tf.split(outputs, num_heads, axis=0), axis=2)  # (N, T_q, C)
+
+        # Residual connection
+        outputs += queries
+
+
+    if with_qk:
+        return Q, K
+    else:
+        return outputs
+
+
+def feedforward(inputs,
+                num_units=[2048, 512],
+                scope="multihead_attention",
+                dropout_keep_prob=1.0,
+                reuse=None):
+    '''
+    Point-wise feed forward net.   feed forward and Add & Norm
+
+    Args:
+      inputs: A 3d tensor with shape of [N, T, C].
+      num_units: A list of two integers.
+      scope: Optional scope for `variable_scope`.
+      reuse: Boolean, whether to reuse the weights of a previous layer
+        by the same name.
+
+    Returns:
+      A 3d tensor with the same shape and dtype as inputs
+    '''
+    with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
+        # Inner layer
+        params = {"inputs": inputs, "filters": num_units[0], "kernel_size": 1,
+                  "activation": tf.nn.relu, "use_bias": True}
+        outputs = tf.layers.conv1d(**params)
+        outputs = tf.nn.dropout(outputs, keep_prob=dropout_keep_prob)
+        # outputs = tf.layers.dropout(outputs, rate=dropout_rate, training=tf.convert_to_tensor(is_training))
+        # Readout layer
+        params = {"inputs": outputs, "filters": num_units[1], "kernel_size": 1,
+                  "activation": None, "use_bias": True}
+        outputs = tf.layers.conv1d(**params)
+        outputs = tf.nn.dropout(outputs, keep_prob=dropout_keep_prob)
+        # outputs = tf.layers.dropout(outputs, rate=dropout_rate, training=tf.convert_to_tensor(is_training))
+
+        # Residual connection
+        outputs += inputs
+
+
+    return outputs
+
